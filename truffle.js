f5397002447b0da9218cd95e10b2cfed4988cf18
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-10-16 13:24:07 +0200

    Update local, ropsten, rinkeby and kovan network configs

--- truffle.js
+++ truffle.js
@@ -5,6 +5,34 @@ module.exports = {
     development: {
       provider: TestRPC.provider({port: 7545}),
       network_id: "*" // Match any network id
-    }
+    },
+    local: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+      gas: 5000000,
+      gasPrice: 2e9, // 2 Gwei
+    },
+    ropsten: {
+      host: 'localhost',
+      port: 8565,
+      network_id: 3,
+      gas: 500000,
+      gasPrice: 10e9, // 10 Gwei
+    },
+    rinkeby: {
+      host: 'localhost',
+      port: 8565,
+      network_id: 4,
+      gas: 500000,
+      gasPrice: 10e9, // 10 Gwei
+    },
+    kovan: {
+      host: 'localhost',
+      port: 8555,
+      network_id: 42,
+      gas: 500000,
+      gasPrice: 10e9, // 10 Gwei
+    },
   }
 };
